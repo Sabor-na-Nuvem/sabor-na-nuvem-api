54c6301919cd59aa2a03c1b876583504f72bc7ec
JoaoSchmitz <jmatheus12349@gmail.com>
2025-11-24 20:11:59 -0300

    fix: retirando carrinhoId do schema de Loja e consertando criação de Loja

---
+++ prisma/migrations/20251124230133_fix_carrinho_id_na_loja/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `carrinhoId` on the `loja` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "public"."loja" DROP COLUMN "carrinhoId";
